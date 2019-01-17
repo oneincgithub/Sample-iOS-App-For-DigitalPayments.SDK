d46b1876f6d97cb54d06f95eca3d24663de5e645
Irina Tolstykh <itolstykh@oneincsystems.com>
2019-01-17 14:56:14 +0500

    DigitalPaymentsSDK added as universal framework

--- Pods/Target Support Files/Pods-DigitalPaymentsSample/Pods-DigitalPaymentsSample-frameworks.sh
+++ Pods/Target Support Files/Pods-DigitalPaymentsSample/Pods-DigitalPaymentsSample-frameworks.sh
@@ -143,10 +143,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/DigitalPaymentsSDK/DigitalPaymentsSDK.framework"
+  install_framework "${PODS_ROOT}/DigitalPaymentsSDK/DigitalPaymentsSDK.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/DigitalPaymentsSDK/DigitalPaymentsSDK.framework"
+  install_framework "${PODS_ROOT}/DigitalPaymentsSDK/DigitalPaymentsSDK.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
